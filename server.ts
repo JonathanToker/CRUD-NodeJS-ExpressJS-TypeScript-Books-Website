a7e14cf5c6d84f6031c3d731c84b7b2c0f117de2
YonatanToker <yonatantoker123@gmail.com>
2024-05-22 17:43:48 +0300

    an ssr layout with ejs

--- server.ts
+++ server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import expressEjsLayouts from "express-ejs-layouts";
+import router from "./routes";
 const app = express();
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
@@ -7,5 +8,5 @@ app.set("layout", "layouts/layout");
 
 app.use(expressEjsLayouts);
 app.use(express.static("public"));
-
+app.use("/", router);
 app.listen(process.env.PORT || 3000);
