d516692125a34fa0709a3a45c6beca633ad62c91
YonatanToker <yonatantoker123@gmail.com>
2024-05-22 19:18:45 +0300

    mongoDB connection string needed

--- server.ts
+++ server.ts
@@ -1,11 +1,21 @@
 import express from "express";
 import expressEjsLayouts from "express-ejs-layouts";
 import router from "./routes";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
-
+if (process.env.DATABASE_URL) {
+  mongoose.connect(process.env.DATABASE_URL);
+} else {
+  console.error("DATABASE_URL environment variable not set!");
+}
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to Mongoose"));
 app.use(expressEjsLayouts);
 app.use(express.static("public"));
 app.use("/", router);
