d20c0dc09c3524d72c3c7c695d559f4a8ae86e29
YonatanToker <yonatantoker123@gmail.com>
2024-05-23 23:56:04 +0300

    correct configurations for nodejs server for vercel

--- routes/index.ts
+++ routes/index.ts
@@ -3,6 +3,6 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("index", { title: "Express" });
+  res.render("index");
 });
 export default router;
